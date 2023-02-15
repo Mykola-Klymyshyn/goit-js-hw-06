7ad4e491b3d64d55a099c727e9b96ba900b5eebe
Mykola-Klymyshyn <klymyshyn33@gmail.com>
2023-02-15 21:01:23 +0200

    +2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ListEl = document.querySelector("#ingredients");
+const createList = content => {
+return content.map(( ingredients) => {
+const itemEl = document.createElement('li');
+itemEl.textContent = ingredients;
+itemEl.classList.add = ("item");
+return itemEl;
+});
+};
+
+ const elements = createList(ingredients);
+ ListEl.append(...elements);
+console.log(ListEl);
