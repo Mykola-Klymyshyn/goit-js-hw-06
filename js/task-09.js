87d68db71a3e7da3b91b138d6cd1e3ee54bbba9c
Mykola-Klymyshyn <klymyshyn33@gmail.com>
2023-02-19 21:03:53 +0200

    finish

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const colorBtn = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+colorBtn.addEventListener("click", () => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+    colorSpan.textContent = color;
+});
